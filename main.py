ed8a12c0f72ad950c7ac99e3305d8bada5120c1e
jjahanip <jahandar.jahani@gmail.com>
2018-07-03 18:58:45 -0400

    change to script mode

--- main.py
+++ main.py
@@ -1,98 +1,142 @@
 import os
+import time
+import pandas as pd
 import numpy as np
 from numpy import linalg as LA
 from scipy import optimize
 import tifffile
 import matplotlib.pyplot as plt
 
-visualize = True
+visualize = False
 
-img_dir = r'E:\\50_plex\\tif'
-src_name = 'S1_R2_C1-C11_A1_C9_IlluminationCorrected_stitched_registered.tif'
-noise_names = ['S1_R4_C1-C11_A1_C6_IlluminationCorrected_stitched_registered.tif',
-               [],
-               []
-               ]
-
-xmin = 22000
-ymin = 13500
-xmax = 26000
-ymax = 14500
+img_dir = r'E:\50_plex\tif'
+script_file = 'script.csv'
 
 
-# read original images
-source = tifffile.imread(os.path.join(img_dir, src_name)).astype(float) / 65535
-noises = []
-for noise_name in noise_names:
-    if noise_name:
-        noises.append(tifffile.imread(os.path.join(img_dir, noise_name)).astype(float) / 65535)
+df = pd.read_csv('script.csv')
+
+# add/update new alphas for each channel
+df["alpha1"] = np.nan if 'alpha1' not in df.columns else None
+df["alpha2"] = np.nan if 'alpha2' not in df.columns else None
+df["alpha3"] = np.nan if 'alpha3' not in df.columns else None
+
+
+for index, row in df.iterrows():
+
+    # if no noise channel is given
+    if row[["channel_1", "channel_2", "channel_3"]].isnull().all():
+        continue
+
+    start = time.time()
+
+    # read parameters
+    src_name = row["filename"]
+    print('==============================================================')
+    print('unmixing image {}'.format(src_name))
+
+    n1_name = row["channel_1"]
+    n2_name = row["channel_2"]
+    n3_name = row["channel_3"]
+
+    # load images
+    source = tifffile.imread(os.path.join(img_dir, src_name)).astype(float) / 65535
+
+    if str(n1_name) == 'nan':
+        noise_1 = np.empty_like(source)
     else:
-        noises.append(np.zeros_like(source))
+        noise_1 = tifffile.imread(os.path.join(img_dir, n1_name)).astype(float) / 65535
 
-# create small crop for optimization
-s = np.copy(source)[ymin:ymax, xmin:xmax]  # crop sample for source image
-n = []
-for noise in noises:
-        n.append(np.copy(noise)[ymin:ymax, xmin:xmax])   # crop sample for noise image
+    if str(n2_name) == 'nan':
+        noise_2 = np.empty_like(source)
+    else:
+        noise_2 = tifffile.imread(os.path.join(img_dir, n2_name)).astype(float) / 65535
+
+    if str(n3_name) == 'nan':
+        noise_3 = np.empty_like(source)
+    else:
+        noise_3 = tifffile.imread(os.path.join(img_dir, n3_name)).astype(float) / 65535
+
+    xmin = int(row["xmin"]) if pd.notna(row['xmin']) else source.shape[1] // 2 - 500
+    ymin = int(row["ymin"]) if pd.notna(row['ymin']) else source.shape[0] // 2 - 500
+    xmax = int(row["xmax"]) if pd.notna(row['xmax']) else source.shape[1] // 2 + 500
+    ymax = int(row["ymax"]) if pd.notna(row['ymax']) else source.shape[0] // 2 + 500
+
+    # create small crop for optimization
+    s = source[ymin:ymax, xmin:xmax]
+    n1 = noise_1[ymin:ymax, xmin:xmax]
+    n2 = noise_2[ymin:ymax, xmin:xmax]
+    n3 = noise_3[ymin:ymax, xmin:xmax]
+
+    # define optimization function
+    def f(params):
+        alpha1, alpha2, alpha3 = params
+        return LA.norm(s - alpha1 * n1 - alpha2 * n2 - alpha3 * n3)
+
+    # optimize paramteres
+    initial_guess = [0, 0, 0]
+    result = optimize.minimize(f, initial_guess, bounds=((0, 1), (0, 1), (0, 1)))
+    alpha1, alpha2, alpha3 = result.x
+    print('\n'.join('alpha_{} = {:.2f}'.format(i, alpha) for i, alpha in enumerate(result.x)))
 
 
-# define optimization function
-def f(params):
-    alpha1, alpha2, alpha3 = params
-    return LA.norm(s - alpha1 * n[0] - alpha2 * n[1] - alpha3 * n[2])
-
-# optimize paramteres
-initial_guess = [0, 0, 0]
-result = optimize.minimize(f, initial_guess, bounds=((0, 1), (0, 1), (0, 1)))
-
-alpha = result.x
-print(alpha)
+    def float_to_uint16(image):
+        image_u16 = np.copy(image) * 65535
+        image_u16[image_u16 < 0] = 0
+        return np.uint16(image_u16)
 
 
-def float_to_uint16(image):
-    image_u16 = np.copy(image) * 65535
-    image_u16[image_u16 < 0] = 0
-    return np.uint16(image_u16)
+    if visualize:
+        c = s - alpha1 * n1 - alpha2 * n2 - alpha3 * n3
+        c = float_to_uint16(c)
 
 
-if visualize:
-    c = s - alpha[0] * n[0] - alpha[1] * n[1] - alpha[2] * n[2]
-    c = float_to_uint16(c)
+        ax1 = plt.subplot(311)
+        plt.imshow(s, cmap='gray')
+        plt.axis('off')
+        plt.title('source')
+
+        plt.subplot(334, sharex=ax1, sharey=ax1)
+        plt.imshow(n1, cmap='gray')
+        plt.axis('off')
+        plt.title('noise')
+
+        plt.subplot(335, sharex=ax1, sharey=ax1)
+        plt.imshow(n2, cmap='gray')
+        plt.axis('off')
+        plt.title('noise')
+
+        plt.subplot(336, sharex=ax1, sharey=ax1)
+        plt.imshow(n3, cmap='gray')
+        plt.axis('off')
+        plt.title('noise')
+
+        plt.subplot(313, sharex=ax1, sharey=ax1)
+        plt.imshow(c, cmap='gray')
+        plt.axis('off')
+        plt.title('corrected')
+
+        plt.show()
+
+    # create and save new image
+    new_img = source - alpha1 * noise_1 - alpha2 * noise_2 - alpha3 * noise_3
+    new_img = float_to_uint16(new_img)
+
+    src_fname = os.path.splitext(src_name)[0]
+    new_name = os.path.join(img_dir, src_fname + '_unmixed.tif')
+    tifffile.imsave(new_name, new_img, bigtiff=True)
+
+    # update alphas with derived values
+    df.loc[index, 'alpha1'] = alpha1
+    df.loc[index, 'alpha2'] = alpha2
+    df.loc[index, 'alpha3'] = alpha3
+
+    end = time.time()
+    df.to_csv(os.path.splitext(script_file)[0] + '_unmixed.csv', index=False)
+    print('time = {}'.format(end - start))
+
+print('==============================================================')
+print('done!')
 
 
-    ax1 = plt.subplot(311)
-    plt.imshow(s, cmap='gray')
-    plt.axis('off')
-    plt.title('source')
 
-    plt.subplot(334, sharex=ax1, sharey=ax1)
-    plt.imshow(n[0], cmap='gray')
-    plt.axis('off')
-    plt.title('noise')
 
-    plt.subplot(335, sharex=ax1, sharey=ax1)
-    plt.imshow(n[1], cmap='gray')
-    plt.axis('off')
-    plt.title('noise')
-
-    plt.subplot(336, sharex=ax1, sharey=ax1)
-    plt.imshow(n[2], cmap='gray')
-    plt.axis('off')
-    plt.title('noise')
-
-    plt.subplot(313, sharex=ax1, sharey=ax1)
-    plt.imshow(c, cmap='gray')
-    plt.axis('off')
-    plt.title('corrected')
-
-    plt.show()
-
-# create and save new image
-new_img = source - alpha[0] * noises[0] - alpha[1] * noises[1] - alpha[2] * noises[2]
-new_img = float_to_uint16(new_img)
-
-src_fname = os.path.splitext(src_name)[0]
-new_name = src_fname + '_unmixed.tif'
-tifffile.imsave(new_name, new_img, bigtiff=True)
-
-print('Done.')
