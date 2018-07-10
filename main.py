5a68275cc119d7db4ef20807743bb94d8334185f
jjahanip <jahandar.jahani@gmail.com>
2018-07-09 20:00:12 -0400

    remove artifact / rescale intensity

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import pandas as pd
 import numpy as np
 from numpy import linalg as LA
 from scipy import optimize
+from skimage import img_as_float, img_as_uint
+from skimage import exposure
 import tifffile
 import matplotlib.pyplot as plt
 
@@ -39,27 +41,26 @@ for index, row in df.iterrows():
     n3_name = row["channel_3"]
 
     # load images
-    source = tifffile.imread(os.path.join(img_dir, src_name)).astype(float) / 65535
+    source = img_as_float(tifffile.imread(os.path.join(img_dir, src_name)))
 
     if str(n1_name) == 'nan':
         noise_1 = np.empty_like(source)
     else:
-        noise_1 = tifffile.imread(os.path.join(img_dir, n1_name)).astype(float) / 65535
-
+        noise_1 = img_as_float(tifffile.imread(os.path.join(img_dir, n1_name)))
     if str(n2_name) == 'nan':
         noise_2 = np.empty_like(source)
     else:
-        noise_2 = tifffile.imread(os.path.join(img_dir, n2_name)).astype(float) / 65535
+        noise_2 = img_as_float(tifffile.imread(os.path.join(img_dir, n2_name)))
 
     if str(n3_name) == 'nan':
         noise_3 = np.empty_like(source)
     else:
-        noise_3 = tifffile.imread(os.path.join(img_dir, n3_name)).astype(float) / 65535
+        noise_3 = img_as_float(tifffile.imread(os.path.join(img_dir, n3_name)))
 
-    xmin = int(row["xmin"]) if pd.notna(row['xmin']) else source.shape[1] // 2 - 500
-    ymin = int(row["ymin"]) if pd.notna(row['ymin']) else source.shape[0] // 2 - 500
-    xmax = int(row["xmax"]) if pd.notna(row['xmax']) else source.shape[1] // 2 + 500
-    ymax = int(row["ymax"]) if pd.notna(row['ymax']) else source.shape[0] // 2 + 500
+    xmin = int(row["xmin"]) if pd.notna(row['xmin']) else 16200
+    ymin = int(row["ymin"]) if pd.notna(row['ymin']) else 6100
+    xmax = int(row["xmax"]) if pd.notna(row['xmax']) else 21300
+    ymax = int(row["ymax"]) if pd.notna(row['ymax']) else 12200
 
     # create small crop for optimization
     s = source[ymin:ymax, xmin:xmax]
@@ -78,17 +79,11 @@ for index, row in df.iterrows():
     alpha1, alpha2, alpha3 = result.x
     print('\n'.join('alpha_{} = {:.2f}'.format(i, alpha) for i, alpha in enumerate(result.x)))
 
-
-    def float_to_uint16(image):
-        image_u16 = np.copy(image) * 65535
-        image_u16[image_u16 < 0] = 0
-        return np.uint16(image_u16)
-
+    # apply to crop
+    c = s - alpha1 * n1 - alpha2 * n2 - alpha3 * n3
+    c = img_as_uint(c)
 
     if visualize:
-        c = s - alpha1 * n1 - alpha2 * n2 - alpha3 * n3
-        c = float_to_uint16(c)
-
 
         ax1 = plt.subplot(311)
         plt.imshow(s, cmap='gray')
@@ -117,13 +112,20 @@ for index, row in df.iterrows():
 
         plt.show()
 
+    # clean artifacts in image
+    max_s = np.max(s)
+    source[source > max_s] = 0
+
     # create and save new image
-    new_img = source - alpha1 * noise_1 - alpha2 * noise_2 - alpha3 * noise_3
-    new_img = float_to_uint16(new_img)
+    corrected_img = source - alpha1 * noise_1 - alpha2 * noise_2 - alpha3 * noise_3
+    corrected_img = img_as_uint(corrected_img)
+
+
+    adjusted_img = exposure.rescale_intensity(corrected_img, in_range='image', out_range='dtype')
 
     src_fname = os.path.splitext(src_name)[0]
     new_name = os.path.join(img_dir, src_fname + '_unmixed.tif')
-    tifffile.imsave(new_name, new_img, bigtiff=True)
+    tifffile.imsave(new_name, adjusted_img, bigtiff=True)
 
     # update alphas with derived values
     df.loc[index, 'alpha1'] = alpha1
